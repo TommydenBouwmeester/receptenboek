765bbaae34b5a0a05bb05c6b7e81eb851eeba44e
Tommy den Bouwmeester <235657655+TommydenBouwmeester@users.noreply.github.com>
2026-02-09 16:42:35 +0100

    Binnen de klasse Ingredient kcal toegevoegd, zodat dit geprint kan worden binnen de main functie (FR-2)

--- ingredient.py
+++ ingredient.py
@@ -1,9 +1,11 @@
 class Ingredient:
-    def __init__(self, naam: str, hoeveelheid: float, eenheid: str):
+    def __init__(self, naam: str, hoeveelheid: float, eenheid: str, kcal: str):
         self.__naam = naam
         self.__hoeveelheid = hoeveelheid
         self.__eenheid = eenheid
+        self.__kcal = kcal
     def __str__(self):
-        return f"- {self.__naam}, {self.__hoeveelheid}, {self.__eenheid}"
+        return f"- {self.__naam}, {self.__hoeveelheid}, {self.__eenheid}, {self.__kcal}"
+    
 
     
