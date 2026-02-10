3f875c1c1ecf59aa662f4243f7b872500fe833d4
Tommy den Bouwmeester <235657655+TommydenBouwmeester@users.noreply.github.com>
2026-02-10 13:29:13 +0100

    nieuwe get en set functies toegevoegd binnen de klasse ingredienten om van buiten de klassen waardes aan te kunnen passen en op te halen

--- ingredient.py
+++ ingredient.py
@@ -4,6 +4,32 @@ class Ingredient:
         self.__hoeveelheid = hoeveelheid
         self.__eenheid = eenheid
         self.__kcal = kcal
+        self.__plantaardig_alternatief= None
+
+
+    def set_hoeveelheid(self, hoeveelheid: float):
+        self.__hoeveelheid = hoeveelheid
+
+    def get_hoeveelheid(self):
+        return self.__hoeveelheid
+    
+    def get_kcal(self):
+        return self.__kcal
+    
+    def get_naam(self):
+        return self.__naam
+    
+    def get_eenheid(self):
+        return self.__eenheid
+    
+    def set_plantaardig_alternatief(self, alternatief):
+        self.__plantaardig_alternatief = alternatief
+    
+    def get_ingredient(self, plantaardig: bool):
+        if plantaardig and self.__plantaardig_alternatief:
+            return self.__plantaardig_alternatief
+        return self
+
     def __str__(self):
         return f"- {self.__naam}, {self.__hoeveelheid} {self.__eenheid}, {self.__kcal} kcal"
     
