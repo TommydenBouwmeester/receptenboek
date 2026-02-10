dff076968b1932430b82b3d686c1253ec146200b
Tommy den Bouwmeester <235657655+TommydenBouwmeester@users.noreply.github.com>
2026-02-10 14:22:57 +0100

    de functie voor het krijgen van het plantaardig recept aangepast, zodat deze hergebruikt kan worden d.m.v een True en False statement

--- recept.py
+++ recept.py
@@ -1,15 +1,14 @@
 from ingredient import Ingredient
 from stap import Stap
 
-
-
 class Recept:
     def __init__(self, naam, omschrijving):
         self.__naam = naam
         self.__omschrijving = omschrijving
         self.__ingredient_list = []
         self.__stappen = []
-    
+        self.__aantal_personen = 1 
+
     def voeg_ingredient_toe(self, ingredient: Ingredient):
         self.__ingredient_list.append(ingredient)
 
@@ -18,8 +17,8 @@ class Recept:
     
     def get_naam(self):
         return self.__naam
-    
-    def voeg_stap_toe(self, stap: Stap):
+
+    def voeg_stap_toe(self, stap):
         self.__stappen.append(stap)
 
     def set_aantal_personen(self, personen: int):
@@ -36,49 +35,48 @@ class Recept:
     
     def get_plantaardig_recept(self, plantaardig: bool):
         totaal_kcal = 0
-        overzicht = "RECEPT: " + self.__naam + "(PLANTAARDIGE VERSIE)" + "\n"
-        overzicht = overzicht + "OMSCHRIJVING:" + self.__omschrijving + "\n\n"
-        overzicht = overzicht + "INGREDIËNTEN:\n"
+        titel = "RECEPT: " + self.__naam
+        if plantaardig:
+            titel = titel + "(PLANTAARDIGE VERSIE)"
 
+        overzicht = titel + "\n"
+        overzicht = overzicht + "OMSCHRIJVING:" + self.__omschrijving + "\n"
+        overzicht = overzicht + "AANTAL PERSONEN: " + str(self.__aantal_personen) + "\n\n"
+        overzicht = overzicht + "INGREDIËNTEN:\n"
         for item in self.__ingredient_list:
             gekozen_ingredient= item.get_ingredient(plantaardig)
             kcal_totaal_ingrediënt = gekozen_ingredient.get_kcal() * self.__aantal_personen
             totaal_kcal = totaal_kcal + kcal_totaal_ingrediënt
-
             naam = gekozen_ingredient.get_naam()
             hoeveelheid = gekozen_ingredient.get_hoeveelheid()
             eenheid = gekozen_ingredient.get_eenheid()
-            
             overzicht = overzicht + f" - {naam}, {hoeveelheid} {eenheid}, {kcal_totaal_ingrediënt} kcal\n"
-
         overzicht = overzicht + "\nBEREIDING:\n"
         teller = 1
-
         for stapje in self.__stappen:
             overzicht = overzicht + str(teller) + ". " + str(stapje) + "\n"
             teller = teller + 1
-
         overzicht = overzicht + "\nTOTALE CALORIEËN: " + str(totaal_kcal)
-
         return overzicht
-
-
-
+    
     def __str__(self):
-        overzicht = "RECEPT: " + self.__naam + "\n"
-        overzicht = overzicht + "OMSCHRIJVING:"  + self.__omschrijving + "\n\n"
+        return self.get_plantaardig_recept(False)
+        
+           
 
-        overzicht = overzicht + "INGREDIËNTEN:\n"
-        for item in self.__ingredient_list:
-            overzicht = overzicht + str(item) + "\n"
 
-        overzicht = overzicht + "\nBEREIDING:\n"
-        teller = 1
-        for stapje in self.__stappen:
-            overzicht = overzicht + str(teller) + ". " + str(stapje) + "\n"
-            teller = teller + 1
-            
-        return overzicht
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     
